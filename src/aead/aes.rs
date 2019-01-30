c556552667301d7007050b415d20b50dcb99c1ce
Jethro Beekman <jethro@fortanix.com>
2019-01-30 11:36:52 +0530

    Disable AES/GCM `Fallback` implementations on SGX

--- src/aead/aes.rs
+++ src/aead/aes.rs
@@ -152,7 +152,7 @@ impl Key {
                 set_encrypt_key!(GFp_vpaes_set_encrypt_key, bytes, key_bits, &mut key)?
             }
 
-            #[cfg(not(target_arch = "aarch64"))]
+            #[cfg(not(any(target_env = "sgx", target_arch = "aarch64")))]
             Implementation::NOHW => {
                 set_encrypt_key!(GFp_aes_nohw_set_encrypt_key, bytes, key_bits, &mut key)?
             }
@@ -183,7 +183,7 @@ impl Key {
             ))]
             Implementation::VPAES_BSAES => encrypt_block!(GFp_vpaes_encrypt, a, self),
 
-            #[cfg(not(target_arch = "aarch64"))]
+            #[cfg(not(any(target_env = "sgx", target_arch = "aarch64")))]
             Implementation::NOHW => encrypt_block!(GFp_aes_nohw_encrypt, a, self),
         }
     }
@@ -280,7 +280,7 @@ impl Key {
                 });
             }
 
-            #[cfg(not(target_arch = "aarch64"))]
+            #[cfg(not(any(target_env = "sgx", target_arch = "aarch64")))]
             Implementation::NOHW => ctr32_encrypt_blocks!(
                 GFp_aes_nohw_ctr32_encrypt_blocks,
                 in_out,
@@ -357,7 +357,7 @@ pub enum Implementation {
     ))]
     VPAES_BSAES = 2,
 
-    #[cfg(not(target_arch = "aarch64"))]
+    #[cfg(not(any(target_env = "sgx", target_arch = "aarch64")))]
     NOHW = 3,
 }
 
@@ -377,11 +377,11 @@ fn detect_implementation(cpu_features: cpu::Features) -> Implementation {
         target_arch = "x86_64",
         target_arch = "x86"
     ))]
-    {
-        if cpu::intel::AES.available(cpu_features) || cpu::arm::AES.available(cpu_features) {
-            return Implementation::HWAES;
+        {
+            if cpu::intel::AES.available(cpu_features) || cpu::arm::AES.available(cpu_features) {
+                return Implementation::HWAES;
+            }
         }
-    }
 
     #[cfg(any(target_arch = "x86_64", target_arch = "x86"))]
     {
@@ -402,12 +402,16 @@ fn detect_implementation(cpu_features: cpu::Features) -> Implementation {
         Implementation::VPAES_BSAES
     }
 
-    #[cfg(not(target_arch = "aarch64"))]
+    #[cfg(target_env = "sgx")]
+    {
+        panic!("No AES implementation available in the SGX environment !")
+    }
+
+    #[cfg(not(any(target_env = "sgx", target_arch = "aarch64")))]
     {
         Implementation::NOHW
     }
 }
-
 #[cfg(test)]
 mod tests {
     use super::{super::BLOCK_LEN, *};
